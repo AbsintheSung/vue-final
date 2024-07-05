aaeb8e633838fcf78ac19f85f7b738bc9585c32d
AbsintheSung <h90243768@gmail>
2024-07-05 10:42:23 +0800

    update:將i標籤改成vue樣式

--- src/layouts/Header.vue
+++ src/layouts/Header.vue
@@ -11,7 +11,7 @@
           <router-link class="nav-item nav-link me-4 active" to="/">Home <span class="sr-only">(current)</span></router-link>
           <router-link class="nav-item nav-link me-4" to="/product">Product</router-link>
           <router-link class="nav-item nav-link me-4" to="detail">Detail</router-link>
-          <router-link class="nav-item nav-link" to="/cart"><i class="fas fa-shopping-cart"></i></router-link>
+          <router-link class="nav-item nav-link" to="/cart"><FontAwesomeIcon :icon="['fas', 'shopping-cart']" /></router-link>
         </div>
       </div>
     </nav>
