2e70c68a2dc2ce9ed515f4b6285383bce856b3c7
AbsintheSung <h90243768@gmail>
2024-07-05 12:03:11 +0800

    feat:進度條引入fontawsome樣式

--- src/components/ProgressLine.vue
+++ src/components/ProgressLine.vue
@@ -3,10 +3,11 @@
   <nav class="navbar navbar-expand-lg navbar-light px-0">
     <a class="navbar-brand" href="./index.html">Navbar</a>
     <ul class="list-unstyled mb-0 ms-md-auto d-flex align-items-center justify-content-between justify-content-md-end w-100 mt-md-0 mt-4">
-      <li class="me-md-6 me-3 position-relative custom-step-line"><i class="fas fa-check-circle d-md-inline d-block text-center"></i> <span class="text-nowrap">Lorem ipsum</span></li>
-      <li class="me-md-6 me-3 position-relative custom-step-line"><i class="fas fa-check-circle d-md-inline d-block text-center"></i> <span class="text-nowrap">Lorem ipsum</span></li>
-      <li><i class="fas fa-dot-circle d-md-inline d-block text-center"></i> <span class="text-nowrap">Lorem ipsum</span></li>
+      <li class="me-md-6 me-3 position-relative custom-step-line"><FontAwesomeIcon class="d-md-inline d-block text-center" :icon="['fas', 'check-circle']" /><span class="text-nowrap">Lorem ipsum</span></li>
+      <li class="me-md-6 me-3 position-relative custom-step-line"><FontAwesomeIcon class="d-md-inline d-block text-center" :icon="['fas', 'check-circle']" /><span class="text-nowrap">Lorem ipsum</span></li>
+      <li><FontAwesomeIcon class="d-md-inline d-block text-center" :icon="['fas', 'dot-circle']" /><span class="text-nowrap">Lorem ipsum</span></li>
     </ul>
   </nav>
 </template>
+
 <style scoped></style>
