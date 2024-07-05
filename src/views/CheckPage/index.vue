cac114a2b19dab9cfd2cd9f3cfde2214a11eb15f
AbsintheSung <h90243768@gmail>
2024-07-05 11:29:54 +0800

    feat:拆分check頁的header部分

--- src/views/CheckPage/index.vue
+++ src/views/CheckPage/index.vue
@@ -1,18 +1,13 @@
-<script setup></script>
+<script setup>
+import Header from "@/layouts/Header.vue";
+import CheckHeader from "@/views/CheckPage/components/CheckHeader.vue";
+</script>
 <template>
   <div class="container">
-    <div class="row justify-content-center">
-      <div class="col-md-10">
-        <nav class="navbar navbar-expand-lg navbar-light px-0">
-          <a class="navbar-brand" href="./index.html">Navbar</a>
-          <ul class="list-unstyled mb-0 ms-md-auto d-flex align-items-center justify-content-between justify-content-md-end w-100 mt-md-0 mt-4">
-            <li class="me-md-6 me-3 position-relative custom-step-line"><i class="fas fa-check-circle d-md-inline d-block text-center"></i> <span class="text-nowrap">Lorem ipsum</span></li>
-            <li class="me-md-6 me-3 position-relative custom-step-line"><i class="fas fa-check-circle d-md-inline d-block text-center"></i> <span class="text-nowrap">Lorem ipsum</span></li>
-            <li><i class="fas fa-dot-circle d-md-inline d-block text-center"></i> <span class="text-nowrap">Lorem ipsum</span></li>
-          </ul>
-        </nav>
-      </div>
-    </div>
+    <CheckHeader>
+      <Header />
+    </CheckHeader>
+
     <div class="row justify-content-center">
       <div class="col-md-10">
         <h3 class="fw-bold mb-4 pt-3">Checkout</h3>
