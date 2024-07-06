7d91f3f71fe66a7f733f48f01a3ac4a19f0b98bf
AbsintheSung <h90243768@gmail.com>
2024-07-07 00:08:20 +0800

    feat:建立homeCarouse資料

--- src/views/home/components/HomeCarousel.vue
+++ src/views/home/components/HomeCarousel.vue
@@ -1,36 +1,73 @@
 <script setup>
 import { ref } from "vue";
-
-const carouselItems = ref([
-  {
-    title: "Lorem ipsum 1",
-    content: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.",
-    author: "Lorem ipsum dolor sit amet 1",
+const courseData = ref({
+  success: true,
+  products: [
+    {
+      category: "測試分類-3",
+      content: "測試的說明-12",
+      description: "測試的描述-12",
+      id: "-O0wlS9bFqst0cDNe-Eg",
+      imageUrl: "https://images.unsplash.com/photo-1516627145497-ae6968895b74?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1924&q=80",
+      imagesUrl: [
+        "https://images.unsplash.com/photo-1516627145497-ae6968895b74?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1924&q=80",
+        "https://images.unsplash.com/photo-1587300003388-59208cc962cb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
+        "https://images.unsplash.com/photo-1517331156700-3c241d2b4d83?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1948&q=80",
+        "https://images.unsplash.com/photo-1617093727343-374698b1b08d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
+        "https://images.unsplash.com/photo-1511914265872-c40672604a80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1867&q=80",
+      ],
+      is_enabled: 1,
+      origin_price: 1000,
+      price: 500,
+      title: "測試的產品-12",
+      unit: "個",
+      num: 1,
+    },
+    {
+      category: "測試分類-3",
+      content: "測試的說明-3",
+      description: "測試的描述-3",
+      id: "-O0vdQuUKONYrLT6QgnO",
+      imageUrl: "https://images.unsplash.com/photo-1516550135131-fe3dcb0bedc7?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=621e8231a4e714c2e85f5acbbcc6a730&auto=format&fit=crop&w=1352&q=80",
+      imagesUrl: [
+        "https://images.unsplash.com/photo-1516627145497-ae6968895b74?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1924&q=80",
+        "https://images.unsplash.com/photo-1587300003388-59208cc962cb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
+        "https://images.unsplash.com/photo-1517331156700-3c241d2b4d83?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1948&q=80",
+        "https://images.unsplash.com/photo-1617093727343-374698b1b08d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
+        "https://images.unsplash.com/photo-1511914265872-c40672604a80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1867&q=80",
+      ],
+      is_enabled: 1,
+      origin_price: 1000,
+      price: 500,
+      title: "測試的產品-3",
+      unit: "個",
+      num: 2,
+    },
+  ],
+  pagination: {
+    total_pages: 1,
+    current_page: 1,
+    has_pre: false,
+    has_next: false,
+    category: "測試分類-3",
   },
-  {
-    title: "Lorem ipsum 2",
-    content: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.",
-    author: "Lorem ipsum dolor sit amet 2",
-  },
-  {
-    title: "Lorem ipsum 3",
-    content: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.",
-    author: "Lorem ipsum dolor sit amet 3",
-  },
-]);
+  messages: [],
+});
 </script>
 <template>
   <div class="bg-light mt-7">
     <div class="container">
       <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
         <div class="carousel-inner">
-          <div v-for="(item, index) in carouselItems" :key="index" class="carousel-item" :class="{ active: index === 0 }">
+          <div v-for="(courseItem, index) in courseData.products" :key="courseItem.id" class="carousel-item" :class="{ active: index === 0 }">
             <div class="row justify-content-center py-7">
               <div class="col-md-6 text-center">
-                <h3>{{ item.title }}</h3>
-                <p class="my-5">{{ item.content }}</p>
+                <h3>
+                  <router-link to="detail">{{ courseItem.title }}</router-link>
+                </h3>
+                <p class="my-5">{{ courseItem.description }}</p>
                 <p>
-                  <small>—{{ item.author }}—</small>
+                  <small>—{{ courseItem.content }}—</small>
                 </p>
               </div>
             </div>
