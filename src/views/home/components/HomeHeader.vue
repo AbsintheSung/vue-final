c7f8441dc55a698c686c61b2c80d73e13d13ddf8
AbsintheSung <h90243768@gmail.com>
2024-07-06 23:26:15 +0800

    update:更換首頁大圖以及大圖內文字

--- src/views/home/components/HomeHeader.vue
+++ src/views/home/components/HomeHeader.vue
@@ -1,14 +1,16 @@
-<script setup></script>
+<script setup>
+import HomeBackgroundImg from "@/assets/images/home_bg2.png";
+</script>
 <template>
   <div class="position-relative">
-    <div class="position-absolute" style="top: 0; bottom: 0; left: 0; right: 0; background-image: url(https://images.unsplash.com/photo-1480399129128-2066acb5009e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80); background-position: center center; opacity: 0.1"></div>
+    <div class="position-absolute" style="top: 0; bottom: 0; left: 0; right: 0; background-position: center center; opacity: 0.1" :style="{ backgroundImage: `url(${HomeBackgroundImg})` }"></div>
     <div class="container d-flex flex-column" style="min-height: 100vh">
       <slot></slot>
       <div class="row justify-content-center my-auto">
         <div class="col-md-4 text-center">
-          <h2>Lorem ipsum.</h2>
-          <p class="text-muted mb-0">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod.</p>
-          <button class="btn btn-dark rounded-0 mt-6">Lorem ipsum.</button>
+          <h2>專業技能線上平台</h2>
+          <p class="text-muted mb-0">就是標榜帶你學到會</p>
+          <button class="btn btn-dark rounded-0 mt-6">點我進入</button>
         </div>
       </div>
     </div>
