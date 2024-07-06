2621eb79a4beeed3eb6c88b7f13d6a1ec6817fb4
AbsintheSung <h90243768@gmail.com>
2024-07-06 11:47:03 +0800

    update:修改HomePage名稱以及各路徑

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ const routes = [
   {
     path: "/",
     name: "HomePage",
-    component: () => import("../views/HomePage/index.vue"),
+    component: () => import("../views/home/index.vue"),
   },
   {
     path: "/product",
---
+++ src/views/home/index.vue
@@ -0,0 +1,21 @@
+<script setup>
+import Header from "@/layouts/Header.vue";
+import HomeHeader from "@/views/home/components/HomeHeader.vue";
+import HomeProductCard from "@/views/home/components/HomeProductCard.vue";
+import HomeCarousel from "@/views/home/components/HomeCarousel.vue";
+import HomeLine from "@/views/home/components/HomeLine.vue";
+import Footer from "@/layouts/Footer.vue";
+</script>
+<template>
+  <HomeHeader>
+    <Header />
+  </HomeHeader>
+
+  <HomeProductCard />
+
+  <HomeCarousel />
+
+  <HomeLine />
+  <Footer />
+</template>
+<style scoped></style>
