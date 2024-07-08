1b17f3742c333615fc3a5a6f18baea57ed499c98
AbsintheSung <h90243768@gmail>
2024-07-08 12:28:14 +0800

    feat:串接單一商品api

--- src/views/detail/index.vue
+++ src/views/detail/index.vue
@@ -5,11 +5,30 @@ import Footer from "@/layouts/Footer.vue";
 // import DetailSwiperard from "@/views/DetailPage/components/DetailSwiperCard.vue";
 import ProductSwiper from "@/components/ProductSwiper.vue";
 import DetailProductItem from "@/views/detail/components/DetailProductItem.vue";
+import axios from "axios";
+import { useRoute } from "vue-router";
+import { onMounted, ref } from "vue";
+const route = useRoute();
+const productId = route.params.productId;
+const baseURL = import.meta.env.VITE_APP_API_URL;
+const apiName = import.meta.env.VITE_APP_API_NAME;
+const oneProductData = ref({});
+const fetchOneProductData = async () => {
+  try {
+    const response = await axios(`${baseURL}/v2/api/${apiName}/product/${productId}`);
+    oneProductData.value = response.data.product;
+  } catch (error) {
+    console.log(error);
+  }
+};
+onMounted(() => {
+  fetchOneProductData();
+});
 </script>
 <template>
   <div class="container">
     <Header />
-    <DetailProductItem />
+    <DetailProductItem :getOneProduct="oneProductData" />
     <ProductSwiper />
   </div>
   <Footer />
