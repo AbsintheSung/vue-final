bc42cce9ce1e99d3985e5c3b7c565d33c1ca1d56
AbsintheSung <h90243768@gmail.com>
2024-07-10 23:50:52 +0800

    feat:安裝vee-validate，並撰寫驗證邏輯

---
+++ src/views/checkout/components/CheckoutForm.vue
@@ -0,0 +1,66 @@
+<script setup>
+import { defineRule, Form, Field, ErrorMessage, configure } from "vee-validate";
+import { all } from "@vee-validate/rules";
+import { localize, setLocale } from "@vee-validate/i18n";
+import zhTW from "@vee-validate/i18n/dist/locale/zh_TW.json";
+
+setLocale("zh_TW"); // 設置 繁體中文
+configure({
+  generateMessage: (context) => {
+    const defaultMessage = localize({
+      zh_TW: zhTW,
+    })(context);
+    const keyName = context.field;
+    return defaultMessage.replace(`${keyName} `, "");
+  },
+  validateOnInput: true,
+});
+const schema = {
+  email: "required|email",
+  userName: "required",
+  phone: "required|numeric",
+  address: "required",
+};
+
+Object.entries(all).forEach(([name, rule]) => {
+  defineRule(name, rule);
+});
+const handleFormConfirm = async (validate) => {
+  const response = await validate();
+  return response.valid ? console.log("跳轉") : null;
+  // console.log(response.valid);
+};
+</script>
+<template>
+  <Form :validation-schema="schema" v-slot="{ validate }">
+    <p>Contact information</p>
+    <div class="mb-2">
+      <label for="ContactMail" class="text-muted mb-0">Email</label>
+      <Field name="email" type="email" class="form-control" id="ContactMail" aria-describedby="emailHelp" placeholder="example@gmail.com" />
+      <ErrorMessage name="email" />
+    </div>
+    <div class="mb-2">
+      <label for="ContactName" class="text-muted mb-0">Name</label>
+      <Field name="userName" type="text" class="form-control" id="ContactName" placeholder="Carmen A. Rose" />
+      <ErrorMessage name="userName" />
+    </div>
+    <div class="mb-2">
+      <label for="ContactPhone" class="text-muted mb-0">Phone</label>
+      <Field name="phone" type="text" class="form-control" id="ContactPhone" placeholder="0911123123" />
+      <ErrorMessage name="phone" />
+    </div>
+    <div class="mb-2">
+      <label for="ContactAddress" class="text-muted mb-0">Address</label>
+      <Field name="address" type="text" class="form-control" id="ContactAddress" placeholder="高雄市三民區" />
+      <ErrorMessage name="address" />
+    </div>
+    <div class="d-flex flex-column-reverse flex-md-row mt-4 justify-content-between align-items-md-center align-items-end w-100">
+      <router-link to="/products" class="text-dark mt-md-0 mt-3">
+        <FontAwesomeIcon class="me-2" :icon="['fas', 'chevron-left']" />
+        回商品頁
+      </router-link>
+      <button @click.prevent="handleFormConfirm(validate)" class="btn btn-dark py-3 px-7">確認</button>
+    </div>
+  </Form>
+</template>
+<style scoped></style>
--- src/views/checkout/index.vue
+++ src/views/checkout/index.vue
@@ -2,6 +2,7 @@
 import ProgressLine from "@/components/ProgressLine.vue";
 import CheckHeader from "@/layouts/CheckHeader.vue";
 import ProductItme from "@/components/ProductItem.vue";
+import CheckoutForm from "@/views/checkout/components/CheckoutForm.vue";
 </script>
 <template>
   <div class="container">
@@ -19,33 +20,7 @@ import ProductItme from "@/components/ProductItem.vue";
         <ProductItme />
       </div>
       <div class="col-md-6">
-        <form>
-          <p>Contact information</p>
-          <div class="mb-0">
-            <label for="ContactMail" class="text-muted mb-0">Email</label>
-            <input type="email" class="form-control" id="ContactMail" aria-describedby="emailHelp" placeholder="example@gmail.com" />
-          </div>
-          <p class="mt-4">Shipping address</p>
-          <div class="mb-2">
-            <label for="ContactName" class="text-muted mb-0">Name</label>
-            <input type="text" class="form-control" id="ContactName" placeholder="Carmen A. Rose" />
-          </div>
-          <div class="mb-2">
-            <label for="ContactPhone" class="text-muted mb-0">Phone</label>
-            <input type="text" class="form-control" id="ContactPhone" placeholder="Password" />
-          </div>
-          <div class="mb-2">
-            <label for="ContactMessage" class="text-muted mb-0">Message</label>
-            <textarea class="form-control" rows="3" id="ContactMessage" placeholder="message ... "></textarea>
-          </div>
-        </form>
-        <div class="d-flex flex-column-reverse flex-md-row mt-4 justify-content-between align-items-md-center align-items-end w-100">
-          <router-link to="/products" class="text-dark mt-md-0 mt-3">
-            <FontAwesomeIcon class="me-2" :icon="['fas', 'chevron-left']" />
-            Lorem ipsum
-          </router-link>
-          <router-link to="/checkoutinfo" class="btn btn-dark py-3 px-7">Lorem ipsum</router-link>
-        </div>
+        <CheckoutForm />
       </div>
     </div>
   </div>
